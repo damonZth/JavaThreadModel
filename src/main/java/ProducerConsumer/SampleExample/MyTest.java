f8c07370a23e849218c6f45f1de5ce968ae94c63
damonZth <822730025@qq.com>
2018-04-22 09:40:17 +0800

    2018 04 22 Java多线程设计模式 初学

---
+++ src/main/java/ProducerConsumer/SampleExample/MyTest.java
@@ -0,0 +1,15 @@
+package ProducerConsumer.SampleExample;
+import org.junit.Test;
+
+/**
+ * Created by Damon on 2018/4/22.
+ */
+public class MyTest {
+
+    @Test
+    public void test() {
+        System.out.println("Test");
+
+    }
+
+}
---
+++ src/main/java/SingleThreadedExecution/SampleExample/MyTest.java
@@ -0,0 +1,15 @@
+package SingleThreadedExecution.SampleExample;
+import org.junit.Test;
+
+/**
+ * Created by Damon on 2018/4/22.
+ */
+public class MyTest {
+
+    @Test
+    public void test() {
+        System.out.println("Test");
+
+    }
+
+}
